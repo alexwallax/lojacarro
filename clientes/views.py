e1fc43af76f1211a6f17bca6a2ee1428dbb2e051
Alex Wallace <alexwallax@gmail.com>
2025-06-19 11:12:42 -0300

    filtrando dados

--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente, Carro
+import re
 
 def clientes(request):
     if request.method == "GET":
@@ -13,7 +15,27 @@ def clientes(request):
         placas = request.POST.getlist('placa')
         anos = request.POST.getlist('ano')
 
-        print(carros)
-        print(placas)
-        print(anos)
+        cliente = Cliente.objects.filter(cpf=cpf)
+
+        if cliente.exists():
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email': email})
+        
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf})
+
+        # criar o cliente
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf,
+        )
+
+        # envia para o banco
+        cliente.save() 
+
+        for carro, placa, ano in zip(carros, placas, anos):
+            car = Carro(carro=carro, placa=placa, ano=ano, cliente=cliente)
+            car.save()
+
         return HttpResponse('teste')
